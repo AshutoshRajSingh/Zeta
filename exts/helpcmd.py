73c3dcee18e54ce7fedf2e744f9f4cbb31a07689
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-02-02 12:39:44 +0530

    New implementation.

--- exts/helpcmd.py
+++ exts/helpcmd.py
@@ -1,84 +1,83 @@
 import discord
 from discord.ext import commands
 
-"""In hindsight it seemed like a good idea now I regret every single aspect of this implementation, might change it."""
+everything = {
+    'level': [
+        'Used to show own or someone else\'s level',
+        "level target",
+        "target here is the member whose level you wish to know (can be mention, id or username), if no target specified, own level is shown"
+    ],
+    'lb': [
+        "Used to show server leaderboard",
+        "lb",
+        ""
+    ],
+
+    'setbd': [
+        "Used to save your own birthday in bot so others can see it",
+        "setbd date_of_birth",
+        "date_of_birth is the date on which you were born, the only acceptable format is:\n`DD MM YYYY`"
+    ],
+    'bday': [
+        "Used to get a member's birthday",
+        "bday target",
+        "target here is the member whose birthday you wish to display, can be mention, id or username"
+    ],
+
+    'prefix': [
+        'Used to set server prefix',
+        "prefix new_prefix",
+        "new_prefix here is the new server prefix that will be used for commands, can be up to 10 characters long"
+    ],
+    'bdchannel': [
+        "Used to set birthday alert channel",
+        "bdaychannel target_channel",
+        "target_channel here is the server channel you wish to set for sending out birthday alerts, if not configured,"
+        "birthday alerts are not sent out."
+    ]
+
+}
+
+categoryinfo = {
+    "Level System": [
+        'level', 'lb'
+    ],
+    "Misc": [
+        'setbd', 'bday'
+    ],
+    "Admin": [
+        'prefix', 'bdchannel'
+    ],
+}
 
 
-@commands.group(invoke_without_command=True)
-async def help(ctx: commands.Context):
-    text = f"""```
-Use {ctx.prefix}help command_name to get info about that command
-        
-{ctx.prefix}level   :   Used to show own or someone else's level
-{ctx.prefix}lb      :   Used to display server leaderboard 
-{ctx.prefix}setbd   :   Saves own birthday in the bot
-{ctx.prefix}bday    :   Sends a member's birthday as message
-```"""
-    embed = discord.Embed(title="Commands",
+@commands.command()
+async def help(ctx: commands.Context, arg: str = None):
+    if not arg:
+        title = "Commands"
+        text = ""
+        for category in categoryinfo:
+            text += f"```{category}```\n"
+            for command in categoryinfo[category]:
+                text += f"**{command}**\n{everything[command][0]}\n\n"
+            text += "\n"
+    else:
+        data = everything.get(arg)
+        if not data:
+            await ctx.send(f"Couldn't find the command that you were looking for, please use `{ctx.prefix}help` to get"
+                           f" a list of usable commands")
+            return
+        title = f"Command: {arg}"
+        text = f"""
+**{data[0]}**
+
+Usage:
+```{ctx.prefix}{data[1]}```
+{data[2]}        
+"""
+    embed = discord.Embed(title=title,
                           description=text,
                           colour=discord.Colour.green())
-    await ctx.send(embed=embed)
-
-
-@help.command()
-async def level(ctx: commands.Context):
-    text = f"""```
-Command used to display own or someone else's level
-    
-Usage:
-{ctx.prefix}level target
-    
-target here is the member whose level you wish to know (can be mention, id or username), if no target specified, own level is shown
-```"""
-    embed = discord.Embed(title="level",
-                          description=text,
-                          colour=discord.Colour.blue())
-    await ctx.send(embed=embed)
-
-
-@help.command()
-async def lb(ctx: commands.Context):
-    text = f"""```
-Command used to display top 10 members with the highest exp points
-
-Usage:
-{ctx.prefix}lb
-```"""
-    embed = discord.Embed(title="lb",
-                          description=text,
-                          colour=discord.Colour.blue())
-    await ctx.send(embed=embed)
-
-
-@help.command()
-async def setbd(ctx: commands.Context):
-    text = f"""```
-Command used to store your birthday in the bot so other server members can see it
-
-Usage:
-{ctx.prefix}setbd date
-
-Date here is your date of birth, the only acceptable format is: DD MM YYYY    
-```"""
-    embed = discord.Embed(title="setbd",
-                          description=text,
-                          colour=discord.Colour.blue())
-    await ctx.send(embed=embed)
-
-
-@help.command()
-async def bday(ctx: commands.Context):
-    text = f"""```
-Command that sends a member's birthday as a message, if they have saved it using the {ctx.prefix}setbd command in this server
-
-Usage:
-{ctx.prefix}bday target
-
-target here is the member whose birthday you wish to see, can be username, id or mention.
-```"""
-    embed = discord.Embed(title="bday",
-                          description=text,
-                          colour=discord.Colour.blue())
     await ctx.send(embed=embed)
 
 
