d6bbaf2503a9100ab42e620afbcd4e96c1f3cf3b
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-02-07 22:14:30 +0530

    very crude implementation of mute command, will improve in time

--- exts/administration.py
+++ exts/administration.py
@@ -1,6 +1,33 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from discord.utils import get
 import asyncio
+
+
+async def create_mute_role(guild: discord.Guild):
+    perms = discord.Permissions.none()
+    newrole = await guild.create_role(name="Muted", permissions=perms)
+
+    ow = discord.PermissionOverwrite(send_messages=False, add_reactions=False)
+
+    for channel in guild.channels:
+        await channel.set_permissions(newrole, overwrite=ow)
+
+    return newrole
+
+QUERY_INTERVAL_MINUTES = 30
+
+
+def parsetime(time: str):
+    arr = time.split(' ')
+    hours = 0
+    minutes = 0
+    for elem in arr:
+        if elem.endswith('h'):
+            hours += int(elem[0:len(elem)-1])
+        elif elem.endswith('m'):
+            minutes += int(elem[0:len(elem) - 1])
+    return {'hours': hours, 'minutes': minutes}
 
 
 class administration(commands.Cog):
@@ -35,6 +62,33 @@ class administration(commands.Cog):
         await ctx.guild.default_role.edit(permissions=discord.Permissions.general())
         await ctx.send(
             embed=discord.Embed(title="Lockdown lifted", colour=discord.Colour.green()))
+
+    @commands.command()
+    @commands.has_guild_permissions(manage_messages=True)
+    async def mute(self, ctx, target: discord.Member, time: str = None):
+        mr = get(ctx.guild.roles, name="Muted")
+        if not mr:
+            await ctx.send("Server doesn't seem to have mute configured yet, stand by please.")
+            async with ctx.channel.typing():
+                mr = await create_mute_role(ctx.guild)
+
+        if not time:
+            await target.add_roles(mr)
+            e = discord.Embed(description=f"{target} has been muted")
+            e1 = discord.Embed(description=f"You have been muted from the server {ctx.guild} indefinitely, you'll only"
+                                           f"be able to send messages if a moderator unmutes you")
+            await ctx.send(embed=e)
+            await target.send(embed=e1)
+
+        if time:
+            pass
+
+    @tasks.loop(minutes=QUERY_INTERVAL_MINUTES)
+    async def mute_poll(self):
+        async with self.bot.pool.acquire() as conn:
+            async with conn.transaction():
+                async for entry in conn.cursor("SELECT "):
+                    pass
 
 
 def setup(bot: commands.Bot):
