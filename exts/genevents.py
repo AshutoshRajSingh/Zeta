34f1f12471b338d6ee0fd2b29ba3ec1484a54b9c
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-03-13 21:41:47 +0530

    Added documentation, guild remove event

--- exts/genevents.py
+++ exts/genevents.py
@@ -1,4 +1,3 @@
-import util
 import discord
 import datetime
 from discord.ext import commands
@@ -7,20 +6,36 @@ from discord.ext import commands
 class GenEvents(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.db = util.DB(bot.pool)
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        await self.db.create_member_table(guild=guild)
+        """
+        Listens for guild joining and then makes database entry
+        """
+        await self.bot.db.create_member_table(guild=guild)
+        await self.bot.db.make_guild_entry(guild.id)
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild: discord.Guild):
+        """
+        Yank out the guild from the database when bot leaves it
+        """
+        await self.bot.db.hakai_guild(guild.id)
 
     @commands.Cog.listener()
     async def on_ready(self):
+        """
+        Called every time a ready event is received, we only need the first time to mark initial readying
+        """
         if not self.bot.initinit:
             print(f"First READY dispatched at {datetime.datetime.utcnow()}")
             self.bot.initinit = True
 
     @commands.Cog.listener('on_message')
     async def ping_reminder(self, message: discord.Message):
+        """
+        If bot is pinged a help message telling about command prefix for that server is shown
+        """
         if message.guild.me in message.mentions:
             prefix = self.bot.prefixes.get(message.guild.id)
             if prefix:
