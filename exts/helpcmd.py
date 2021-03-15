c78bb8c2c019215db7d689d5280b0fe57d58a951
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-03-15 23:45:41 +0530

    Now embeds help messages.

--- exts/helpcmd.py
+++ exts/helpcmd.py
@@ -2,14 +2,58 @@ import discord
 from discord.ext import commands
 
 
-# Will customize later.
 class MyHelp(commands.MinimalHelpCommand):
     def __init__(self):
         super().__init__()
 
+    @staticmethod
+    def _get_command_signature(command):
+        return f"`{command.qualified_name}`"
 
-hc = MyHelp()
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Commands",
+                              description=f"Use {self.clean_prefix}help [command] for more info on a command.\nYou can also use {self.clean_prefix}help [category] for more info on a category.",
+                              colour=discord.Colour.purple())
+        for cog, commands in mapping.items():
+            filtered = await self.filter_commands(commands, sort=True)
+            command_signatures = [self._get_command_signature(c) for c in filtered]
+            if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "Other")
+                embed.add_field(name=cog_name, value=", ".join(command_signatures))
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_command_help(self, command):
+        embed = discord.Embed(title="Command: "+command.qualified_name,
+                              description=f"Usage:```{self.get_command_signature(command)}```\n"+command.help,
+                              colour=discord.Colour.purple())
+        alias = command.aliases
+        if alias:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_cog_help(self, cog):
+
+        cmds = cog.get_commands()
+        desc = "\n\n".join([f"{cmd.name} - {cmd.help.splitlines()[0]}" for cmd in cmds])
+        e = discord.Embed(title=f"{cog.qualified_name}",
+                          description=cog.description + "\n\n" + desc,
+                          colour=discord.Colour.purple())
+        chan = self.get_destination()
+        await chan.send(embed=e)
+
+    async def send_group_help(self, group):
+        subcmds = "\n".join([f"{self.clean_prefix}{group.qualified_name} {cmd.name} - {cmd.help.splitlines()[0]}" for cmd in group.commands])
+        desc = f"Usage: ```{self.clean_prefix}{group.qualified_name} {group.signature}``` \n {group.help}\n\n Use {self.clean_prefix}help [command] for more info on a command.\nYou can also use {self.clean_prefix}help [category] for more info on a category.\n\n"+"**Commands:**"
+        e = discord.Embed(title=f"{group.qualified_name}",
+                          description=desc+"\n"+subcmds,
+                          colour=discord.Colour.purple())
+        chan = self.get_destination()
+        await chan.send(embed=e)
 
 
 def setup(bot: commands.Bot):
-    bot.help_command = hc
+    bot.help_command = MyHelp()
