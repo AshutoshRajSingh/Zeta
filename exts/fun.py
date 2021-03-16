3a9e4c90e63399ef932e2a04db9774e4768b30f1
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-03-16 13:37:26 +0530

    fixed edge cases where video posts were considered valid

--- exts/fun.py
+++ exts/fun.py
@@ -1,7 +1,7 @@
 import time
 import random
-import aiohttp
 import discord
+import asyncio
 from discord.ext import commands
 
 
@@ -15,8 +15,8 @@ class Fun(commands.Cog):
     @commands.command(aliases=['r'])
     async def reddit(self, ctx: commands.Context, subreddit: str):
         """
-        Fetches a random hot post from a subreddit.
-        Only works for image posts duh, and doesn't send any posts marked nsfw.
+        Fetches a random hot image-post from a subreddit.
+        Doesn't send any post marked nsfw.
         """
         BASE = 'https://reddit.com'
         ROUTE = BASE + '/r/%s.json'
@@ -30,8 +30,9 @@ class Fun(commands.Cog):
         # Count the number of nsfw/video posts in the subreddit as they don't go to discord
         nsfw = 1
         video = 1
+        misc_invalid = 1
 
-        # Max number of posts to look up
+        # Max number of searches to do
         MAX_LOOKUPS = 25
 
         # The reddit json api doesn't (seem to) return a 404 if subreddit doesn't exist, this seems like a workaround
@@ -39,36 +40,81 @@ class Fun(commands.Cog):
         if len(d['data']['children']) == 0:
             await ctx.send("Subreddit not found! It may be private or might not exist.")
             return
-        # There was an attempt to ignore video/nsfw posts however for the video part, if the sub is a video only sub
-        # bad things happen and I really am not in the mood to investigate what's wrong (apparently the is_video bool
-        # is incorrect for some posts)
+
+        # This bit makes sure that the selected post is not a video/marked nsfw, basically keeps choosing a post
+        # randomly up to a specified limit : MAX_LOOKUPS until a proper image post is hit.
         else:
             posts = d['data']['children']
-            while nsfw < MAX_LOOKUPS and video < MAX_LOOKUPS:
+            while nsfw < MAX_LOOKUPS and video < MAX_LOOKUPS and misc_invalid < MAX_LOOKUPS:
                 selected = random.choice(posts)
+                if selected['data'].get('url_overridden_by_dest') is None:
+                    misc_invalid += 1
+                    continue
+
+                # We take the most "relevant" image formats :shrug:
+                if selected['data']['url_overridden_by_dest'].endswith(('jpg', 'png')):
+                    e = discord.Embed(title=selected['data']['title'],
+                                      url=BASE + selected['data']['permalink'],
+                                      colour=discord.Colour.red())
+                    e.set_image(url=selected['data']['url_overridden_by_dest'])
+
+                    duration = time.time() - start
+                    e.set_footer(text=f'Requested by {ctx.author}, fetched in %.2gs' % duration,
+                                 icon_url=ctx.author.avatar_url)
+
+                    await ctx.send(embed=e)
+                    return
                 if selected['data']['over_18']:
                     nsfw += 1
-                    continue
-                if selected['data']['is_video']:
+                if selected['data']['is_video'] or selected['data']['url_overridden_by_dest'].startswith('https://v.redd.it/'):
+                    # To make the command compatible with video posts, here you probably can send the video url in the channel
+                    # embeds do not support videos so I decided to not do this as I wanted video to be in embed too.
+                    # Haven't tried just sending the video url into the channel thought it probably might work.
                     video += 1
-                    continue
 
-                e = discord.Embed(title=selected['data']['title'],
-                                  url=BASE+selected['data']['permalink'],
-                                  colour=discord.Colour.red())
-                e.set_image(url=selected['data']['url_overridden_by_dest'])
-
-                duration = time.time()-start
-                e.set_footer(text=f'Requested by {ctx.author}, fetched in %.2gs' % duration,
-                             icon_url=ctx.author.avatar_url)
-
-                await ctx.send(embed=e)
-                return
-
+            # Send apropriate error message
             if nsfw >= MAX_LOOKUPS:
-                await ctx.send(f"Looked up {MAX_LOOKUPS} posts, all were nsfw, not posting")
+                await ctx.send(f"Seems like you gave me an nsfw subreddit, can't post.")
             elif video >= MAX_LOOKUPS:
-                await ctx.send(f"Looked up {MAX_LOOKUPS} posts, all were videos, can't post.")
+                await ctx.send(f"Seems like you gave me a video only subreddit, can't post.")
+            elif misc_invalid >= MAX_LOOKUPS:
+                await ctx.send(f'Seems like you gave me a text based subreddit, can\'t post')
+
+    @commands.command(aliases=['doggo', 'doge'])
+    async def dog(self, ctx: commands.Context):
+        """
+        Sends a picture of a random dog.
+        """
+        BASE = 'https://random.dog'
+        ROUTE = '/woof.json'
+        async with self.bot.cs.get(BASE+ROUTE) as r:
+            d = await r.json()
+
+        e = discord.Embed(title="Random dog",
+                          colour=discord.Colour(0xFFB6C1))
+        e.set_image(url=d.get('url'))
+        if d.get('url').endswith(('.mp4', 'webm')):
+            await ctx.send(d.get('url'))
+            return
+        await ctx.send(embed=e)
+
+    @commands.command(aliases=['catto', 'cate'])
+    async def cat(self, ctx: commands.Context):
+        """
+        Sends a picture of a random cat.
+        """
+        BASE = 'http://aws.random.cat'
+        ROUTE = '/meow'
+        async with self.bot.cs.get(BASE + ROUTE) as r:
+            d = await r.json()
+
+        e = discord.Embed(title="Random cat",
+                          colour=discord.Colour(0xFFB6C1))
+        e.set_image(url=d.get('file'))
+        if d.get('file').endswith(('.mp4', 'webm')):
+            await ctx.send(d.get('file'))
+            return
+        await ctx.send(embed=e)
 
 
 def setup(bot: commands.Bot):
