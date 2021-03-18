faaebcace793e5f9088bb0af83ebeff835c47dba
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-03-18 17:30:56 +0530

    updated with new tables

--- autodb.py
+++ autodb.py
@@ -32,10 +32,12 @@ async def init(Url):
 
     queries = {
         'guilds': "CREATE TABLE guilds (id bigint PRIMARY KEY, prefix varchar(10), bdayalert bigint, bdayalerttime time)",
-        'mutes': "CREATE TABLE mutes (id bigint, guildid bigint, mutedtill timestamp)",
-        'tags': "CREATE TABLE tags (name varchar(100), authorid bigint, guildid bigint, content text)"
+        'mutes': "CREATE TABLE mutes (id bigint, guildid bigint, mutedtill timestamp, FOREIGN KEY (guildid) REFERENCES guilds(id) ON DELETE CASCADE)",
+        'tags': "CREATE TABLE tags (name varchar(100), authorid bigint, guildid bigint, content text, FOREIGN KEY (guildid) REFERENCES guilds(id) ON DELETE CASCADE)",
+        'server_members': "CREATE TABLE server_members (guildid bigint, memberid bigint, level int, exp bigint, boost int, birthday date, PRIMARY KEY (guildid, memberid), FOREIGN KEY (guildid) REFERENCES guilds(id) ON DELETE CASCADE)",
+        'selfrole_lookup': "CREATE TABLE selfrole_lookup (guildid bigint, messageid bigint, channelid bigint, FOREIGN KEY (guildid) REFERENCES guilds(id) ON DELETE CASCADE)",
+        'selfrole': "CREATE TABLE selfrole (messageid bigint, emoji varchar(30), roleid bigint, FOREIGN KEY (messageid) REFERENCES selfrole_lookup(messageid) ON DELETE CASCADE)"
     }
-
     count = 0
     for key, value in queries.items():
         try:
