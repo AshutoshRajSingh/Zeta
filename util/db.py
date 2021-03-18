82826eac82cf4acf35b66e7cc1c7c8aedafa99aa
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-03-18 17:09:38 +0530

    Reject multiple table embrace composite primary key

--- util/db.py
+++ util/db.py
@@ -7,37 +7,6 @@ class DB:
         self.pool = pool
         self.remove_guild = self.hakai_guild
 
-    async def create_member_table(self, **kwargs):
-        """
-        Function to create a member table in the database for a particular guild, accepts both the guild as in a
-        discord.Guild object or simply the id of a guild, both accepted as keyword arguments, if table already exists,
-        it is ignored silently and nothing is changed. ie the table isn't created.
-
-        kwargs:
-            guild : the discord.Guild object\n
-            guild_id : the id of the guild
-        """
-        if 'guild' in kwargs:
-            guild_id = kwargs['guild'].id
-        elif 'guild_id' in kwargs:
-            guild_id = kwargs['guild_id']
-        else:
-            raise ValueError("Guild id / guild not supplied.")
-
-        if type(guild_id) is not int:
-            raise TypeError("Guild id must be int")
-
-        async with self.pool.acquire() as con:
-            await con.execute(f"CREATE TABLE IF NOT EXISTS server_members{guild_id} ("
-                              f"id bigint, "
-                              f"username varchar(255), "
-                              f"level int, "
-                              f"exp int, "
-                              f"ispaused boolean, "
-                              f"boost int, "
-                              f"birthday date, "
-                              f"CONSTRAINT fk_guildid FOREIGN KEY (id) REFERENCES  guilds(id) ON DELETE CASCADE)")
-
     async def make_guild_entry(self, guild_id):
         """
         Creates an entry for a guild in the guilds table, if already exists, gets ignored silently\n
@@ -65,23 +34,8 @@ class DB:
         """
         await self.pool.execute("DELETE FROM guilds WHERE id = $1", guild_id)
 
-    async def remove_guild_table(self, guild_id: int):
-        """
-        Yeets a guild table
-        Args:
-            guild_id: id of the guild to delete the table of
-
-        Returns:
-            None
-
-        """
-        if type(guild_id) is not int:
-            raise TypeError("'guild_id' must be int")
-        await self.pool.execute(f"DROP TABLE server_members{guild_id}")
-
     async def hakai_guild(self, guildid: int):
         await self.remove_guild_entry(guildid)
-        await self.remove_guild_table(guildid)
 
     async def fetch_member(self, guildid, memberid):
         """
@@ -120,13 +74,8 @@ class DB:
         """
 
         async with self.pool.acquire() as conn:
-            table_name = "server_members" + str(guildid)
-            if type(guildid) is not int:
-                raise TypeError('guildid must be int')
-
-            query = f"INSERT INTO {table_name} (id, username, level, exp, ispaused, boost) " \
-                    f"VALUES ($1, $2, $3, $4, $5, $6)"
-            await conn.execute(query, memberid, 'deprecated', 0, 0, False, 1)
+            query = "INSERT INTO server_members (guildid, memberid, level, exp, boost, birthday) VALUES ($1, $2, $3, $4, $5, $6)"
+            await conn.execute(query, guildid, memberid, 0, 0, 1, None)
 
     async def fetch_guild_selfole_data(self, guildid):
         data = {}
@@ -140,3 +89,5 @@ class DB:
                     else:
                         data[entry.get('messageid')][entry.get('emoji')] = entry.get('roleid')
         return data
+
+
