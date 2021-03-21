8f02aaf2409876d77ccb0465990e72116e477a49
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-03-21 23:33:05 +0530

    Subclassed for no apparent reason

--- main.py
+++ main.py
@@ -6,61 +6,60 @@ import datetime
 import traceback
 from discord.ext import commands
 
-print(f"All dates/time are in UTC unless stated otherwise\n"
-      f"Started process at {datetime.datetime.utcnow()}")
 
-intents = discord.Intents.all()
+class Zeta(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix=self.get_pre, **kwargs)
+        print(f"All dates/time are in UTC unless stated otherwise\n"
+              f"Started process at {datetime.datetime.utcnow()}")
+        util.startup.start(self)
+        self.load_exts()
+        self.prefixes = {}
+        self.guild_prefs = {}
+        self.plugins = ['levelling', 'birthdays']
+        self.initinit = False
+        self.token = kwargs.get('token')
+
+    async def get_pre(self, _bot: commands.Bot, message: discord.Message):
+        if message.guild is None:
+            return '.'
+        prefix = self.prefixes.get(message.guild.id)
+        if prefix:
+            return prefix
+        else:
+            return "."
+
+    def load_exts(self):
+        extensions = [
+            'guildconfig',
+            'genevents',
+            'commanderrorhandler',
+            'levelsystem',
+            'moderation',
+            'birthdaysystem',
+            'reactionroles',
+            'utility',
+            'fun',
+            'misc',
+            'helpcmd',
+            '_jishaku',
+        ]
+        print("Loading extensions...")
+        for ext in extensions:
+            if ext.startswith('_'):
+                ext = ext[1:len(ext)]
+            else:
+                ext = 'exts.' + ext
+            try:
+                self.load_extension(ext)
+                print(f"{ext} OK")
+            except commands.ExtensionFailed as error:
+                print(f"{ext} FAIL")
+                traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+    def kickstart(self):
+        self.run(self.token)
 
 
-async def get_pre(_bot: commands.Bot, message: discord.Message):
-    if message.guild is None:
-        return '.'
-    prefix = _bot.prefixes.get(message.guild.id)
-    if prefix:
-        return prefix
-    else:
-        return "."
-
-
-bot = commands.Bot(command_prefix=get_pre, intents=intents, help_command=None)
-bot.prefixes = {}
-bot.guild_prefs = {}
-bot.plugins = ['levelling', 'birthdays']
-bot.initinit = False
-
-# This takes care of doing the stuff that needs to happen as bot starts, for example establishing a connection
-# to the database, loading server prefixes etc.
-util.startup.start(bot)
-
-"""---Call this inefficient but I like being able to exclude any extension by simply commenting out that line ree---"""
-
-# Extensions inside exts dir named as is, extensions that are a site-package prefixed with '_' here currently jishaku
-extensions = [
-    'guildconfig',
-    'genevents',
-    'commanderrorhandler',
-    'levelsystem',
-    'moderation',
-    'birthdaysystem',
-    'reactionroles',
-    'utility',
-    'fun',
-    'misc',
-    'helpcmd',
-    '_jishaku',
-]
-
-print("Loading extensions...")
-for ext in extensions:
-    if ext.startswith('_'):
-        ext = ext[1:len(ext)]
-    else:
-        ext = 'exts.' + ext
-    try:
-        bot.load_extension(ext)
-        print(f"{ext} OK")
-    except commands.ExtensionFailed as error:
-        print(f"{ext} FAIL")
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-
-bot.run(os.environ['BOT_TOKEN'])
+bot = Zeta(token=os.environ['BOT_TOKEN'])
+bot.kickstart()
