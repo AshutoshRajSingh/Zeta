7cb24e42276a502d790e83109586aae0dc8d478d
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2021-04-11 11:42:16 +0530

    drop menus in favor of non paginated help

--- exts/helpcmd.py
+++ exts/helpcmd.py
@@ -1,94 +1,6 @@
 import discord
-from discord.ext import commands, menus
+from discord.ext import commands
 
-class bothelp(menus.Menu):
-    """
-    Menu for bot help
-    """
-    def __init__(self, mapping: dict, prefix):
-        self.clean_prefix = prefix
-        self.embeddesc = f"Use {self.clean_prefix}help [command] for more info on a command.\nYou can also use {self.clean_prefix}help [category] for more info on a category."
-        self.baseembed = discord.Embed(title="Commands", description=self.embeddesc, colour=discord.Colour.purple())
-        self.mapping = mapping
-        self.coglist = []
-        self.current_page = 1
-        self.listinate()
-        super().__init__()
-
-    def get_next_page(self):
-        """
-        Returns next page based off the current page, a "page" is nothing but a list of dicts with cog name mapped to commands
-        """
-        return self.coglist[3*self.current_page: 3*(self.current_page+1)]
-
-    def listinate(self):
-        """
-        Takes the mapping of commands recieved and yoinks them into a list.
-        """
-        for cog, cmds in self.mapping.items():
-            try:
-                if len(cog.get_commands()) == 0:
-                    continue
-                if cog.qualified_name.lower() == 'jishaku':
-                    continue
-            except AttributeError:
-                self.coglist.append({"Other": cmds})
-                continue
-            self.coglist.append({cog.qualified_name: cmds})
-
-    async def send_initial_message(self, ctx, channel):
-        current = self.coglist[0:3]
-        e = self.baseembed
-        for item in current:
-            for cog, cmds in item.items():
-                if type(cog) is str:
-                    e.add_field(name=cog, value=" ".join([f"`{cmd.name}`" for cmd in cmds]), inline=False)
-                else:
-                    e.add_field(name=cog.qualified_name, value=" ".join([f"`{cmd.name}`" for cmd in cmds]), inline=False)
-        return await channel.send(embed=e)
-
-    @menus.button("\U000025c0")
-    async def backward(self, payload):
-        """
-        Two steps back and one step forward, ig that counts
-        """
-        self.current_page -= 2
-        if self.current_page < 0:
-            self.current_page = 1
-            return
-        await self.forward(payload)
-
-    @menus.button("\U000023f9")
-    async def stopme(self, payload):
-        """
-        Stops menu
-        """
-        await self.message.delete()
-        await self.stop()
-
-    @menus.button("\U000025b6")
-    async def forward(self, payload):
-        """
-        One page forward.
-        """
-        current = self.get_next_page()
-        if not current:
-            return
-
-        e = self.baseembed
-        e.clear_fields()
-        for item in current:
-            for cog, cmds in item.items():
-                if type(cog) is str:
-                    e.add_field(name=cog,
-                                value=" ".join([f"`{cmd.name}`" for cmd in cmds]),
-                                inline=False)
-                else:
-                    e.add_field(name=cog.qualified_name,
-                                value=" ".join([f"`{cmd.name}`" for cmd in cmds]),
-                                inline=False)
-        await self.message.edit(embed=e)
-        self.current_page += 1
 
 class MyHelp(commands.MinimalHelpCommand):
     def __init__(self):
@@ -100,8 +12,35 @@ class MyHelp(commands.MinimalHelpCommand):
         return f"`{command.qualified_name}`"
 
     async def send_bot_help(self, mapping):
-        m = bothelp(mapping, self.clean_prefix)
-        await m.start(self.context)
+        ignored_cogs = ['jishaku']
+        addeddesc = f"""
+        Use `{self.clean_prefix}list` to get a list of all the user commands.
+        Use `{self.clean_prefix}help category` to list all commands in that category with a little info. 
+        Use `{self.clean_prefix}help command` to get detailed information about using a command.     
+        
+        _Note that command and category names are case sensitive_       
+        """
+        e = discord.Embed(title="Help",
+                          description=addeddesc,
+                          colour=0xFFB6C1)
+        for cog, cmds in mapping.items():
+
+            if cog is not None:
+                if len(cog.get_commands()) == 0 or cog.qualified_name.lower() in ignored_cogs:
+                    continue
+                name = cog.qualified_name
+                if cog.description:
+                    value = cog.description
+                else:
+                    value = "No desc found"
+            else:
+                name = None
+                value = None
+
+            if name is not None and value is not None:
+                e.add_field(name=name, value=value, inline=False)
+        chan = self.get_destination()
+        await chan.send(embed=e)
 
     async def send_command_help(self, command):
         embed = discord.Embed(title="Command: " + command.qualified_name,
